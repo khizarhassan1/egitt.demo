da3640e245083907f310dcc171fac17f007505a1
FA12-BSE-124 <FA12-BSE-124@LAB7C-05.cns.ciit>
2015-11-09 10:31:27 +0500

    1sr commit

---
+++ src/egit/demo/main.java
@@ -0,0 +1,12 @@
+package egit.demo;
+
+public class main {
+
+	public static void main( String[] arg) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Lets Do Something Using GIT");
+
+	}
+
+}
