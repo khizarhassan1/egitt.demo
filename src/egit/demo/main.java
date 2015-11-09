f2e822aaef98edc411f647df1f2e5d313bf70cb0
FA12-BSE-124 <FA12-BSE-124@LAB7C-05.cns.ciit>
2015-11-09 11:23:01 +0500

    stay away

--- src/egit/demo/main.java
+++ src/egit/demo/main.java
@@ -2,11 +2,11 @@ package egit.demo;
 
 public class main {
 
-	public static void main( String[] arg) 
+	public static main( String[] arg) 
 	{
 		// TODO Auto-generated method stub
 		System.out.println("Lets Do Something Using GIT");
-
+		System.out.println("Lets Do Something ABOVE");
 	}
 
 }
